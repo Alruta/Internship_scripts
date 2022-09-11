4a0a794bad9ab84ca45ed3fe12c849e573a4a244
Alvaro Ruiz Tabas <107360960+Alruta@users.noreply.github.com>
2022-09-11 14:22:11 +0200

    Update AUTO-CM-builder.sh

--- AUTO-CM-builder.sh
+++ AUTO-CM-builder.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-n=0
+COUNTER=0
 for f in *.db;
 	do 
 		echo "                                                                         "
 		echo "============================ ""$f"" CM BUILDING BEGIN ""============================"
 		echo "                                                                         "
 		mkdir "$f""_CM"
-		perl /media/atabaz/DISCO_DURO_ART/IIMCB_internship/PROJECTS/SARS_GENOME_PROJECT/INFERNAL/labtools/cm-builder -m "$f" -d /media/atabaz/DISCO_DURO_ART/IIMCB_internship/PROJECTS/SARS_GENOME_PROJECT/GENOMES/CoV_DataBase/BIG_CoV_DATABASE.fasta -s /media/atabaz/DISCO_DURO_ART/IIMCB_internship/PROJECTS/SARS_GENOME_PROJECT/GENOMES/NC_045512.2-SARS_CoV.fasta -k -c 4 -r -o "$f""_CM"
-		n=$n+1
+		perl path/to/cm-builder -m "$f" -d path/to/database.fasta -s path/to/reference.fasta -k -c core_number -r -o "$f""_CM"
+		let COUNTER++
 	done
 
 echo "                                                                         "
 echo "============================"" ALL DONE ""============================"
-echo "Number of files analyzed: ""$n"
+echo "Number of files analyzed: ""$COUNTER"
 echo "                                                                         "
